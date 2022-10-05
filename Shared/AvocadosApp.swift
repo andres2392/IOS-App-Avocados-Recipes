f348c988c59834863a11afb15df194d06401dd44
andres2392 <32281805+andres2392@users.noreply.github.com>
2022-10-05 09:51:36 -0400

    Initial Commit

---
+++ Shared/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Shared
+//
+//  Created by PDWS on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
